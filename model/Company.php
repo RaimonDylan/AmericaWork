44691e52a8ab0269c69c59a4c43ebaf89e97c94c
lucascencini <cencinilucas@gmail.com>
2019-03-15 13:51:18 +0100

    Classe company complété

--- model/Company.php
+++ model/Company.php
@@ -27,6 +27,41 @@ class Company extends Mysql
         $this->typeSector = $typeSector;
     }
 
+    function insert()
+    {
+        $variable = $this->base->prepare("INSERT INTO company (name,addr,city,pc,nbEmploye,typeSector) VALUES (:name, :addr, :city, :pc, :nbEmploye, :typeSector)");
+        $variable->execute(array("name" => $this->name, "addr" => $this->addr, "city" => $this->city, "pc" => $this->pc, "nbEmploye" => $this->nbEmploye, "typeSector" => $this->typeSector));
+    }
+
+    function update()
+    {
+        $variable = $this->base->prepare("UPDATE company SET name = :name, addr = :addr, city = :city, pc = :pc, nbEmploye = :nbEmploye, typeSector = :typeSector WHERE id_company = :id_company");
+        $variable->execute(array("login" => $this->login, "password" => $this->password, "dt_ins" => $this->dt_ins, "surname" => $this->surname, "name" => $this->name, "tel" => $this->tel, "mail" => $this->mail, "addr" => $this->addr, "city" => $this->city, "pc" => $this->pc, "id_user" => $this->id_user));
+    }
+
+    function show()
+    {
+        echo "<tr>
+                <td>$this->id_company</td>
+                <td>$this->name</td>
+                <td>$this->addr</td>
+                <td>$this->city</td>
+                <td>$this->pc</td>
+                <td>$this->nbEmploye</td>
+                <td>$this->typeSector</td>
+                <td>
+                    <i data-id = '$this->id_company' class='fa fa-edit update' style='cursor: pointer;margin-right:3%;'></i>
+                    <i data-id = '$this->id_company' class='fa fa-trash-alt delete' style='cursor: pointer;margin-left:3%;'></i>
+                </td>
+              </tr>";
+    }
+
+    function delete()
+    {
+        $variable = $this->base->prepare("DELETE FROM company WHERE id_company = :id_company");
+        $variable->execute(array('id_company'=> $this->id_company));
+    }
+
     /**
      * @return mixed
      */
