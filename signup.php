ac3a2549edd4607ed422f2ef5f1b5e1a8e0c0615
lucascencini <cencinilucas@gmail.com>
2019-05-24 11:33:40 +0200

    Ajout sécurité inscription

--- signup.php
+++ signup.php
@@ -18,39 +18,39 @@ include_once 'includes/header.php';
 			<div class="panel-body">
 				<div class="form-group">
 					<label class="control-label">Nom d'utilisateur</label>
-					<input type="text" name="login" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+					<input type="text" required="required" name="login" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
 				</div>
 				<div class="form-group">
 					<label class="control-label">Mot de passe</label>
-					<input type="password" name="password" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+					<input type="password" required="required" name="password" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
 				</div>
                 <div class="form-group">
                     <label class="control-label">Nom</label>
-                    <input type="text" name="l_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" required="required" name="l_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Prénom</label>
-                    <input type="text" name="f_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" required="required" name="f_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Téléphone</label>
-                    <input type="tel"  name="phone" class="form-control" readonly onfocus="this.removeAttribute('readonly');" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
+                    <input type="tel" required="required" name="phone" class="form-control" readonly onfocus="this.removeAttribute('readonly');" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Adresse mail</label>
-                    <input type="email" name="email" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="email" required="required" name="email" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Adresse</label>
-                    <input type="text" name="address" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" required="required" name="address" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Ville</label>
-                    <input type="text" name="city" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" required="required" name="city" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Code postal</label>
-                    <input type="text" maxlength="5" pattern="[0-9]{5}" name="pc" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" required="required" maxlength="5" pattern="[0-9]{5}" name="pc" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <input class="typeSignup" type="radio" name="choice" value="student"> Élève<br>
@@ -59,23 +59,23 @@ include_once 'includes/header.php';
                 <div style="display: none;" class="student">
                     <div class="form-group">
                         <label class="control-label">Site web</label>
-                        <input type="url" name="siteweb" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                        <input type="url" required="required" name="siteweb" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                     </div>
                     <div class="form-group">
                         <label class="control-label">Description</label>
-                        <textarea type="text" name="description" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                        <textarea type="text" rows="4" cols="50" name="description" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                     </div>
                     <div class="form-group">
                         <label class="control-label">Twitter</label>
-                        <input type="text" name="twitter" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                        <input type="url" name="twitter" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                     </div>
                     <div class="form-group">
                         <label class="control-label">Facebook</label>
-                        <input type="text" name="facebook" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                        <input type="url" name="facebook" class="form-control" readonly onfocus="this.removeAttribute('readonly');" pattern="(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-]*)?">
                     </div>
                     <div class="form-group">
                         <label class="control-label">Hobbies</label>
-                        <input type="text" name="hobbies" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                        <textarea type="text" rows="4" cols="50" name="hobbies" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                     </div>
                 </div>
                 <button type="submit" class="btn btn-success loginField" >Valider</button>
