43323fe83b21617c7e293653163aac40a84dba3d
lucascencini <cencinilucas@gmail.com>
2019-05-24 10:52:06 +0200

    Modification de la page inscription rajout des éléments nécessaire

--- signup.php
+++ signup.php
@@ -18,21 +18,65 @@ include_once 'includes/header.php';
 			<div class="panel-body">
 				<div class="form-group">
 					<label class="control-label">Nom d'utilisateur</label>
-					<input type="text" name="login" class="form-control" required="required" readonly onfocus="this.removeAttribute('readonly');">
+					<input type="text" name="login" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
 				</div>
 				<div class="form-group">
 					<label class="control-label">Mot de passe</label>
-					<input type="password" name="password" class="form-control" required="required" readonly onfocus="this.removeAttribute('readonly');">
+					<input type="password" name="password" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
 				</div>
                 <div class="form-group">
                     <label class="control-label">Nom</label>
-                    <input type="text" name="l_name" class="form-control" required="required" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" name="l_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
                 <div class="form-group">
                     <label class="control-label">Prénom</label>
-                    <input type="password" name="f_name" class="form-control" required="required" readonly onfocus="this.removeAttribute('readonly');">
+                    <input type="text" name="f_name" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
                 </div>
-				<button type="submit" class="btn btn-success loginField" >Valider</button>
+                <div class="form-group">
+                    <label class="control-label">Téléphone</label>
+                    <input type="text" name="phone" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Adresse mail</label>
+                    <input type="text" name="email" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Adresse</label>
+                    <input type="text" name="address" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Ville</label>
+                    <input type="text" name="city" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Code postal</label>
+                    <input type="text" name="pc" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <input type="radio" name="type" value="student" checked> Élève<br>
+                    <input type="radio" name="type" value="recruiter"> Recruteur<br>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Site web</label>
+                    <input type="text" name="siteweb" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Description</label>
+                    <input type="text" name="description" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Twitter</label>
+                    <input type="text" name="twitter" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Facebook</label>
+                    <input type="text" name="facebook" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Hobbies</label>
+                    <input type="text" name="hobbies" class="form-control" readonly onfocus="this.removeAttribute('readonly');">
+                </div>
+                <button type="submit" class="btn btn-success loginField" >Valider</button>
 			</div>
 		</div>
 	</form>
